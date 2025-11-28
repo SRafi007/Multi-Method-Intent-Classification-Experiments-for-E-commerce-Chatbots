c8a8804673a1be2f5e9b38e3b49bee8594520d22
SRafi007 <sadmansakib.rafiaiub@gmail.com>
2025-11-28 23:08:18 +0600

    fixed inference file

--- ml_classifier/inference.py
+++ ml_classifier/inference.py
@@ -1,19 +1,19 @@
 import pickle
 from preprocessing import clean_text
 
-with open("models/classifier.pkl", "rb") as f:
-    model = pickle.load(f)
+with open("ml_classifier/models/classifier.pkl", "rb") as f:
+    obj = pickle.load(f)
     clf = obj["model"]
     embedder = obj["embedder"]
 
 def predict_intent(query:str)->str:
     cleaned = clean_text(query)
     vec = embedder.encode([cleaned])
-    pred = clf.redict(vec)[0]
+    pred = clf.predict(vec)[0]
     return pred
 
 if __name__ == "__main__":
     while True:
-        q = intent("User: ")
+        q = input("User: ")
         print("intent: ", predict_intent(q))
     
